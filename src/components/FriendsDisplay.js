c40e08c5e8c68d77ef92968241a464b21e35c37a
Ude Prince Kenechukwu <154351338+kenechiude@users.noreply.github.com>
2024-09-20 20:10:05 +0100

    Updated FriendsDisplay.js

--- src/components/FriendsDisplay.js
+++ src/components/FriendsDisplay.js
@@ -2,19 +2,34 @@ import { useState } from "react";
 import Friend from "./Friend";
 import FriendForm from "./FriendForm";
 
-export default function FriendsDisplay({ friends, setFriends }) {
-  const [formIsOpen, setFomIsOpen] = useState(false);
+export default function FriendsDisplay({
+  handleSelectBtn,
+  friends,
+  setFriends,
+}) {
+  const [friendFormIsOpen, setFriendformIsOpen] = useState(false);
+  const [clicked, setClicked] = useState(null);
 
   function handleDisplay() {
-    setFomIsOpen(!formIsOpen);
+    setFriendformIsOpen(!friendFormIsOpen);
+  }
+
+  function handleBtnDisplay(id) {
+    setClicked(id === clicked ? null : id);
   }
 
   return (
     <div className="friend-div">
       {friends.map((friend) => (
-        <Friend friend={friend} key={friend.id} />
+        <Friend
+          handleSelectBtn={handleSelectBtn}
+          handleBtnDisplay={handleBtnDisplay}
+          clicked={clicked}
+          friend={friend}
+          key={friend.id}
+        />
       ))}
-      {formIsOpen ? (
+      {friendFormIsOpen ? (
         <FriendForm
           handleDisplay={handleDisplay}
           friends={friends}

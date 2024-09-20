7677eca9edf12f9ae693669aef2727ad0d3181c5
Ude Prince Kenechukwu <154351338+kenechiude@users.noreply.github.com>
2024-09-20 20:07:45 +0100

    Updated BillForm.js

--- src/components/BillForm.js
+++ src/components/BillForm.js
@@ -1,19 +1,28 @@
-export default function BillForm() {
+import { useState } from "react";
+
+export default function BillForm({ activeFriend }) {
+  const [formData, setFormData] = useState({
+    billValue: 0,
+    yourExpense: 0,
+    friendExpense: 0,
+    payer: "",
+  });
+
   return (
     <div className="bill-form">
       <form className="b-f-form">
-        <p className="b-f-p">Split a bill with X</p>
+        <p className="b-f-p">Split a bill with {activeFriend.name}</p>
         <div className="b-f-form-div">
           <span>💰 Bill Value</span>
           <input type="number" />
           <span>🕺 Your Expense</span>
           <input type="number" />
-          <span>👨🏽‍🤝‍👨🏼 X's Expense</span>
+          <span>👨🏽‍🤝‍👨🏼 {activeFriend.name}'s Expense</span>
           <input type="number" />
           <span>🤑 Who's paying?</span>
           <select>
             <option value="you">You</option>
-            <option value={"x"}>X</option>
+            <option value={activeFriend.name}>{activeFriend.name}</option>
           </select>
           <div></div>
           <button className="sp-btn">Split Bill</button>

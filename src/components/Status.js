d2a29fbd0d277e74a6cd550fe09c7cfeb32f9fb7
Ude Prince Kenechukwu <154351338+kenechiude@users.noreply.github.com>
2024-09-22 12:50:05 +0100

    Updated Status.js

--- src/components/Status.js
+++ src/components/Status.js
@@ -2,9 +2,8 @@ export default function Status({ total, name }) {
   if (!total) return <span className="ash">You and {name} are even 😁</span>;
   else
     return total > 0 ? (
-      <span className="grn">{`${name} owes you $${total}`}</span>
+      <span className="rd">{`You owe ${name} $${total}`}</span>
     ) : (
-      <span className="rd">{`You owe ${name} $${Math.abs(total)}`}</span>
+      <span className="grn">{`${name} owes you $${Math.abs(total)}`}</span>
     );
 }
-
